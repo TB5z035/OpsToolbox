ddc2085296aaf1c7e2891e88e5283de798ba0f9c
tb5zhh <tianbeiwen88@gmail.com>
2021-12-26 13:43:30 +0800

    support more input

--- slack/send.sh
+++ slack/send.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DEFAULT=$1
+DEFAULT=$*
 TEXT=${DEFAULT:-"knock knock"}
 curl -X POST --data-urlencode "payload={\"channel\": \"#notification\", \"username\": \"webhookbot\", \"text\": \"$TEXT\", \"icon_emoji\": \":bulb:\"}" $HOOK_URL > /dev/null 2>&1
 
