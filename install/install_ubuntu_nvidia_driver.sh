a12b47ec61c808567e16f6db750b9fc2480b2b4a
George Tian <tianbeiwen88@gmail.com>
2022-05-29 13:42:13 +0800

    Update install_ubuntu_nvidia_driver.sh
    update pubkey of Nvidia

--- install/install_ubuntu_nvidia_driver.sh
+++ install/install_ubuntu_nvidia_driver.sh
@@ -6,10 +6,7 @@ apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin -P /tmp
 mv /tmp/cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/3bf863cc.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | tee /etc/apt/sources.list.d/cuda.list
 apt-get update
 apt-get -y install cuda-drivers
-
-echo "export PATH=/usr/local/cuda-11.5/bin${PATH:+:${PATH}}" > /etc/profile
-echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.5/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}" > /etc/profile
