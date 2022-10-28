f65af611c971f172ca35f906f4750bdc2f7eb441
George Tian <tianbeiwen88@gmail.com>
2022-10-28 11:07:00 +0800

    update activate.sh

--- proxy/activate.sh
+++ proxy/activate.sh
@@ -1,7 +1,8 @@
-PORT=50000
-export HTTP_PROXY=http://localhost:$PORT
-export HTTPS_PROXY=http://localhost:$PORT
-export ALL_PROXY=http://localhost:$PORT
-export http_proxy=http://localhost:$PORT
-export https_proxy=http://localhost:$PORT
-export all_proxy=http://localhost:$PORT
+HOST=localhost
+PORT=61111
+export HTTP_PROXY=http://$HOST:$PORT
+export HTTPS_PROXY=http://$HOST:$PORT
+export ALL_PROXY=http://$HOST:$PORT
+export http_proxy=http://$HOST:$PORT
+export https_proxy=http://$HOST:$PORT
+export all_proxy=http://$HOST:$PORT
